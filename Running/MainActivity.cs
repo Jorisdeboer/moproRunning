3bb5b768da88888b0b8166d5472fb9350df28969
Jorisdeboer <jorisdeboer97@gmail.com>
2017-12-06 15:33:48 +0100

    start of project

---
+++ Running/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Android.Graphics;
+using Android.Content;
+
+namespace Running
+{
+    [Activity(Label = "Running", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle b)
+        {
+            base.OnCreate(b);
+            LinearLayout layout;
+            layout = new LinearLayout(this);
+            
+            SetContentView(layout);
+        }
+
+    }
+    
+    public class RunningView : View
+    {
+        public RunningView(Context c) : base(c)
+        {
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+        }
+    }
+}
+
