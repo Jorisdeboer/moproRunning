5af67e843dde3ce64c81d6a81d921114811da93a
Jorisdeboer <jorisdeboer97@gmail.com>
2017-12-06 16:59:56 +0100

    update1
    basic layout UI gedaan

--- Running/Resources/Resource.Designer.cs
+++ Running/Resources/Resource.Designer.cs
@@ -41,11 +41,30 @@ namespace Running
 			}
 		}
 		
-		public partial class Layout
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int Utrecht = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Utrecht2 = 2130837505;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
@@ -60,8 +79,8 @@ namespace Running
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
